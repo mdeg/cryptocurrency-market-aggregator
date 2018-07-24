b93e89ab72c9c4d0701bde788b4b95e0a751e4b1
Michael Degano <michael@dega.no>
2018-07-24 16:29:45 +1000

    Use serde_rename to unify broadcast names

--- src/broadcast_api/mod.rs
+++ src/broadcast_api/mod.rs
@@ -8,52 +8,45 @@ pub type Volume = i64;
 pub type Total = i64;
 
 #[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
 pub enum Broadcast {
     #[serde(rename = "hb")]
     Heartbeat {},
-    #[serde(rename = "orderbookUpdate")]
     OrderbookUpdate {
         source: Exchange,
         pair: CurrencyPair,
         bids: Vec<(Price, Volume)>,
         asks: Vec<(Price, Volume)>
     },
-    #[serde(rename = "orderbookRemove")]
     OrderbookRemove {
         source: Exchange,
         pair: CurrencyPair,
         bids: Vec<(Price, Volume)>,
         asks: Vec<(Price, Volume)>
     },
-    #[serde(rename = "orderbookSnapshot")]
     OrderbookSnapshot {
         source: Exchange,
         pair: CurrencyPair,
         bids: Vec<(Price, Volume)>,
         asks: Vec<(Price, Volume)>
     },
-    #[serde(rename = "tradeSnapshot")]
     TradeSnapshot {
         source: Exchange,
         pair: CurrencyPair,
         trades: Vec<(Timestamp, Price, Volume, Total)>
     },
-    #[serde(rename = "trade")]
     Trade {
         source: Exchange,
         pair: CurrencyPair,
         trade: (Timestamp, Price, Volume, Total)
     },
-    #[serde(rename = "connected")]
     Connected {
         multiplier: i32
     },
-    #[serde(rename = "connectionOpened")]
     ExchangeConnectionOpened {
         exchange: Exchange,
         ts: Timestamp
     },
-    #[serde(rename = "connectionClosed")]
     ExchangeConnectionClosed {
         exchange: Exchange,
         ts: Timestamp
